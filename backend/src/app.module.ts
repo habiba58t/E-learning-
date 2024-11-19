f9a164e714175f97685039269a990c8aa6878565
habiba58t <habibatalha2004@gmail.com>
2024-11-19 18:38:51 +0300

    progress schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProgressModule } from './progress/progress.module';
 
 @Module({
-  imports: [],
+  imports: [ProgressModule],
   controllers: [AppController],
   providers: [AppService],
 })
