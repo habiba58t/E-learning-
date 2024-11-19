cb3bdffbb43c50c6b8c1293244f05c60ff39e298
hagaradel12 <hagaradel198@gmail.com>
2024-11-19 21:59:05 +0200

    modules schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProgressModule } from './progress/progress.module';
 import { UsersModule } from './users/users.module';
 import { CoursesModule } from './courses/courses.module';
+import { ModulesModule } from './modules/modules.module';
 
 @Module({
-  imports: [ProgressModule, UsersModule, CoursesModule],
+  imports: [ProgressModule, UsersModule, CoursesModule, ModulesModule],
   controllers: [AppController],
   providers: [AppService],
 })
