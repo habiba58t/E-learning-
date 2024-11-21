b0aca90d2c06c9a244b68131ea3f45bde3edb891
hagaradel12 <hagaradel198@gmail.com>
2024-11-21 14:01:34 +0200

    Db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,25 +5,14 @@ import { ProgressModule } from './progress/progress.module';
 import { UsersModule } from './users/users.module';
 import { CoursesModule } from './courses/courses.module';
 import { ModulesModule } from './modules/modules.module';
-<<<<<<< HEAD
-import { TestModule } from './test/test.module';
-
-@Module({
-  imports: [ProgressModule, UsersModule, CoursesModule, ModulesModule, TestModule],
-=======
-<<<<<<< HEAD
+import { ResponsesModule } from './responses/responses.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { QuizzesModule } from './quizzes/quizzes.module';
 import { QuestionsModule } from './questions/questions.module';
+import {NotesModule} from './notes/notes.module'
 
 @Module({
-  imports: [ProgressModule, UsersModule, CoursesModule, ModulesModule, QuizzesModule, QuestionsModule],
-=======
-import { ResponsesModule } from './responses/responses.module';
-
-@Module({
-  imports: [ProgressModule, UsersModule, CoursesModule, ModulesModule, ResponsesModule],
->>>>>>> 141e217546d4e1454e6f994358b48356cf6e5037
->>>>>>> eefc138bfc7a5cb319ae6c82587476ff50628665
+  imports: [ProgressModule, UsersModule, CoursesModule, ModulesModule, ResponsesModule, QuizzesModule, QuestionsModule,NotesModule,MongooseModule.forRoot('mongodb://localhost:27017/E-Learning')],
   controllers: [AppController],
   providers: [AppService],
 })
