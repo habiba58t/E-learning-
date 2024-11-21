c01d66f442ae186e6c25045691b0ad19d10a7d2f
farahh212 <farahhany9779@gmail.com>
2024-11-21 14:34:56 +0200

    with response array

--- backend/src/quizzes/quizzes.schema.ts
+++ backend/src/quizzes/quizzes.schema.ts
@@ -1,14 +1,21 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { Question } from '../questions/questions.schema'; // Import Question schema
+import { Responses } from '../responses/responses.schema'; // Import Responses schema
 
 @Schema()
-export class Quiz extends Document {
+export class Quiz extends mongoose.Document {
   @Prop({
     type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
     required: true,
   }) // Reference to Question schema using ObjectId
   questions: mongoose.Schema.Types.ObjectId[];
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Responses' }],
+    default: [],
+  }) // Reference to Responses schema using ObjectId
+  responses: mongoose.Schema.Types.ObjectId[];
 
   @Prop({ default: Date.now }) // Automatically set to the current date/time
   created_at: Date;
