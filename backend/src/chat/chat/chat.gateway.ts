11187a78476b4073e4a82ca94fa6325c343be9ae
ialiaaah <aliaaelhwari@gmail.com>
2024-12-03 22:51:13 +0200

    chats one to one

--- backend/src/chat/chat/chat.gateway.ts
+++ backend/src/chat/chat/chat.gateway.ts
@@ -1,5 +1,5 @@
-
-import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer } from '@nestjs/websockets';
+// src/communication/chat/chat.gateway.ts
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer, ConnectedSocket } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
 import { Server, Socket } from 'socket.io';
 
@@ -11,19 +11,33 @@ export class ChatGateway {
 
   // Listen for the 'send_message' event from the client
   @SubscribeMessage('send_message')
-  async handleMessage(@MessageBody() message: { message: string; userId: string }): Promise<void> {
+  async handleMessage(@MessageBody() message: { message: string; userId: string; recipientId: string }, @ConnectedSocket() client: Socket): Promise<void> {
     // Save the message to the database
-    const savedMessage = await this.chatService.saveMessage(message.message, message.userId);
+    const savedMessage = await this.chatService.saveMessage(message.message, message.userId, message.recipientId);
 
-    // send to all connected clients
-    this.server.emit('receive_message', savedMessage);
+    // Send the message to the recipient only
+    const recipientSocketId = this.getRecipientSocketId(message.recipientId);
+    if (recipientSocketId) {
+      this.server.to(recipientSocketId).emit('receive_message', savedMessage);
+    }
   }
 
-  //fetch all chats
+  // Get all messages between two users
   @SubscribeMessage('get_messages')
-  async getMessages(@MessageBody() data: any): Promise<void> {
-    const messages = await this.chatService.getMessages();
-    this.server.emit('receive_messages', messages);  // send all ll client
+  async getMessages(@MessageBody() data: { userId: string; recipientId: string }): Promise<void> {
+    const messages = await this.chatService.getMessages(data.userId, data.recipientId);
+    const clientSocketId = this.getRecipientSocketId(data.userId);
+    this.server.to(clientSocketId).emit('receive_messages', messages);
+  }
+
+  // Helper function to get the recipient's socket ID
+  getRecipientSocketId(userId: string): string | undefined {
+    const clients = this.server.sockets.sockets;
+    for (const client of clients.values()) {
+      if (client.handshake.query.userId === userId) {
+        return client.id;
+      }
+    }
+    return undefined;
   }
 }
-// one to one chat
